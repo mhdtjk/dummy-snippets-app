7f655af6a844f803bbe3d4c3fcaca030fe4cf004
mhdtjk <mehrdad.tjk@gmail.com>
2024-09-04 14:36:41 +0330

    snippet by id page: - fix id page: style and links

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { db } from "@/db"
+import Link from "next/link"
 import { notFound } from "next/navigation"
 
 interface pageProps {
@@ -15,6 +16,18 @@ export default async function page(props: pageProps) {
     if (!snippet) return notFound()
 
     return (
-        <div>{snippet?.title}</div>
+        <div>
+            <div className="flex justify-between items-center py-4">
+                <h3 className="text-xl font-bold">{snippet?.title}</h3>
+                <div className="flex gap-2">
+                    <Link href={`/snippets/${snippet.id}/edit`} className="p-2 border rounded">Edit</Link>
+                    <button className="p-2 border rounded">Delete</button>
+                </div>
+            </div>
+
+            <pre className="p-4 border rounded bg-gray-100">
+                <code>{snippet.code}</code>
+            </pre>
+        </div>
     )
-}
+} 
